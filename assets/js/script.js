07dd3ca6d356049bfb5d52e446053ca9d7163003
akashebaev-ux <akashebaev@gmail.com>
2026-03-23 16:46:25 +0100

    API settings

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const API_KEY= "AXU_9ZdtspMEY7XTpAI5N0gAvYc";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e=> getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    } else {
+        throw new Error(data.error);
+    }
+}
